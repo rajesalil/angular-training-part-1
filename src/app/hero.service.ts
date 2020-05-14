b50dc18c5ba611aa4621e665ffcc6a5e88e11bc1
rajesalil <rajesalil5@gmail.com>
2020-05-14 12:39:11 +0530

    2 files modified

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
 import {Hero} from '../hero';
 import {HEROES} from './mock-heroes';
+
 
 @Injectable({
   providedIn: 'root',
@@ -9,8 +12,8 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes():Hero[]{
-  return HEROES;
+  getHeroes():Observable<Hero[]>{
+  return of(HEROES);
 }
 
 }
