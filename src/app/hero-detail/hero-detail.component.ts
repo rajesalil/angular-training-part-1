841ded34b0cbf2a1c7c16e1200f29782bf4f410f
rajesalil <rajesalil5@gmail.com>
2020-05-15 13:31:35 +0530

    1 file modified

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -20,13 +20,17 @@ export class HeroDetailComponent implements OnInit {
   private heroService: HeroService,
   private location: Location
 ) {}
-
-  ngOnInit() {
+  
+  ngOnInit(): void {
     this.getHero();
   }
 
   getHero(): void {
-    const id = this.route.snapshot.paramMap.get('id');
+    const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
