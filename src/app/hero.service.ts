2d928162559eea06195e8c0e30e243f500208ac5
rajesalil <rajesalil5@gmail.com>
2020-05-18 13:52:20 +0530

    2 files modified

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,6 +13,9 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
   private heroesUrl = 'api/heroes';  // URL to web api
+  httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(private http: HttpClient,private messageService: MessageService) { }
 
@@ -39,7 +42,7 @@ getHero(id: number): Observable<Hero> {
 /** PUT: update the hero on the server */
 updateHero(hero: Hero): Observable<any> {
   return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
-    tap(_ => this.log(`updated hero id=${hero.id}`)),
+    //tap(_ => this.log(`updated hero id=${hero.id}`)),
     catchError(this.handleError<any>('updateHero'))
   );
 }
